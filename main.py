5ccce2cfa3fe5ac0f84af71c1708fa353c7d904b
Jose P. Leitao <jose.p.leitao@gmail.com>
2025-01-11 10:40:29 -0300

    Created the project and first TODO list

---
+++ main.py
@@ -0,0 +1,13 @@
+"""This is experimentation project with Deep Learning
+goal: Make a frontend to choose a dataset, explore, tunning the model
+and make some predictions"""
+#TODO Define the imports
+#TODO Obtain list of datasets
+#TODO Create the interface
+#TODO list them, so the user can choose the dataset
+#TODO load the choosed dataset
+#TODO Display description of Dataset
+#TODO Clean the Dataset
+#TODO Choose the model
+#TODO Training the model
+#TODO Make some prediction
